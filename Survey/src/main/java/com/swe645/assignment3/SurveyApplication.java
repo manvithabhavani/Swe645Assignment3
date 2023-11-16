e65d6fd3334f101a05ba7b421fecb16235b3d193
Sagar21999 <51937895+Sagar21999@users.noreply.github.com>
2023-11-16 17:33:20 -0500

    updated to jar file

--- Survey/src/main/java/com/swe645/assignment3/SurveyApplication.java
+++ Survey/src/main/java/com/swe645/assignment3/SurveyApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 @EntityScan("com.swe645.assignment3.entity")
-public class SurveyApplication extends SpringBootServletInitializer {
+public class SurveyApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SurveyApplication.class, args);
